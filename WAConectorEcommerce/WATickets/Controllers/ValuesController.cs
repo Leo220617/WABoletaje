1d65dfb13ae729ef3d6b67b7a8ecc792a2366576
Leo220617 <32038650+Leo220617@users.noreply.github.com>
2022-09-15 15:19:24 -0600

    cambios abt

---
+++ WAConectorEcommerce/WATickets/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WATickets.Models;
+using WATickets.Models.Cliente;
+
+namespace WATickets.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        ModelCliente db = new ModelCliente();
+
+        public async Task<HttpResponseMessage> Get([FromUri] Filtros filtro)
+        {
+            try
+            {
+                int resp = Conexion.Company.Connect();
+                if (resp != 0)
+                {
+                    var Respuesta = Conexion.Company.GetLastErrorDescription();
+                    Conexion.Desconectar(); 
+                    return Request.CreateResponse(HttpStatusCode.OK, Respuesta);
+                 
+                }
+                else
+                {
+                    Conexion.Desconectar();
+
+                    return Request.CreateResponse(HttpStatusCode.OK, resp.ToString());
+
+                   
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StackTrace = ex.StackTrace;
+                be.Fecha = DateTime.Now;
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+    }
+}
